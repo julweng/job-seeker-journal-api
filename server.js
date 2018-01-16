58b25c5422e842b8c41d49f37dbf3cf5e14a8cdc
julweng <julweng@gmail.com>
2018-01-16 15:22:42 -0800

    set test to /

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/api/*', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ok: true});
 });
+
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
