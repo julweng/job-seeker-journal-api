1e00ecf22862b24e3d40338de8ab4e13d039730d
julweng <julweng@gmail.com>
2018-01-24 13:55:32 -0800

    set up user and auth

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const app = express();
+ const app = express();
 
-const PORT = process.env.PORT || 3000;
+ const PORT = process.env.PORT || 3000;
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
-});
+ app.get('/api/*', (req, res) => {
+   res.json({ok: true});
+ });
 
-const cors = require('cors');
-const {CLIENT_ORIGIN} = require('./config');
+ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
-
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-module.exports = {app};
+ module.exports = {app};
---
+++ user/model.js
@@ -0,0 +1,39 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {type: String, default: ''},
+  lastName: {type: String, default: ''}
+});
+
+UserSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    username: this.username || ''
+  };
+};
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt
+    .compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 3);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
